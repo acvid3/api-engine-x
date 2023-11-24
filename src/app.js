4451787c5cb070ea587026a0e8ca2b2b733966ba
1 <a1@MacBook-Air-1.local>
2023-11-24 20:07:41 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
