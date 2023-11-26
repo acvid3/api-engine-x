d1ac9ce51b47c1f495f23261a31c0e614ff81509
acvid3 <acvid3@gmail.com>
2023-11-26 21:08:19 +0200

    fix bug get collections

--- src/services/puppeteer-manager.js
+++ src/services/puppeteer-manager.js
@@ -9,7 +9,26 @@ class PuppeteerManager {
     console.log(token, expiryTime, Math.floor(Date.now() / 1000));
 
     const browser = await puppeteer.launch({ headless: true });
-    this.sessions[token] = { browser, expiryTime };
+    const page = await browser.newPage();
+    await page.setExtraHTTPHeaders({
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Cache-Control": "max-age=0",
+      Connection: "keep-alive",
+      "Upgrade-Insecure-Requests": "1",
+      Referer: "https://www.google.com/",
+      DNT: "1",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
+    });
+
+    await page.goto("https://opensea.io/rankings", {
+      waitUntil: "networkidle0",
+    });
+
+    this.sessions[token] = { browser, page, expiryTime };
 
     const timeLeft = expiryTime * 1000 - Date.now();
     if (timeLeft <= 0) {
@@ -29,6 +48,7 @@ class PuppeteerManager {
   async closeSession(token) {
     if (this.sessions[token]) {
       console.log(`Closing session for token: ${token}`);
+      await this.sessions[token].page.close();
       await this.sessions[token].browser.close();
       delete this.sessions[token];
     }
--- src/utils/get-collections.js
+++ src/utils/get-collections.js
@@ -1,62 +1,5 @@
 const puppeteerManager = require("../services/puppeteer-manager");
 
-const findDeepestElementsByText = (
-  selector,
-  textArray,
-  timeout = 5000,
-  interval = 10,
-  caseSensitive = false
-) => {
-  return new Promise((resolve, reject) => {
-    const startTime = Date.now();
-
-    const checkElement = () => {
-      const elements = document.querySelectorAll(selector);
-      let foundElements = [];
-
-      for (let element of elements) {
-        for (let text of textArray) {
-          const elementText = caseSensitive
-            ? element.textContent.trim()
-            : element.textContent.trim().toLowerCase();
-          const searchText = caseSensitive
-            ? text.trim()
-            : text.trim().toLowerCase();
-
-          if (elementText.includes(searchText)) {
-            let childHasSameText = Array.from(element.children).some(
-              (child) => {
-                const childText = caseSensitive
-                  ? child.textContent.trim()
-                  : child.textContent.trim().toLowerCase();
-                return childText.includes(searchText);
-              }
-            );
-
-            if (!childHasSameText) {
-              foundElements.push(element);
-            }
-          }
-        }
-      }
-
-      if (foundElements.length > 0 || Date.now() - startTime > timeout) {
-        if (foundElements.length > 0) {
-          resolve(foundElements);
-        } else {
-          console.log("No elements found");
-          reject(new Error("Timeout exceeded"));
-        }
-        return;
-      }
-
-      setTimeout(checkElement, interval);
-    };
-
-    checkElement();
-  });
-};
-
 async function getCollections(token, last_update, sort) {
   const session = puppeteerManager.getSession(token);
 
@@ -64,14 +7,11 @@ async function getCollections(token, last_update, sort) {
     throw new Error("Session not font");
   }
 
-  const page = await session.browser.newPage();
-  await page.goto("https://opensea.io/rankings", { waitUntil: "networkidle0" });
+  const page = session.page;
 
-  await page.screenshot({ path: `./scrapingbee_homepage.jpg` });
   await page.waitForSelector("#__NEXT_DATA__");
-  const collectionsData = await page.evaluate(() => {
-    findDeepestElementsByText("body", ["Yes"])[0].click();
 
+  const collectionsData = await page.evaluate(() => {
     const rawData = document.getElementById("__NEXT_DATA__").innerText;
     const data = JSON.parse(rawData);
     const initialRecords = data.props.pageProps.initialRecords;
@@ -86,8 +26,6 @@ async function getCollections(token, last_update, sort) {
     }
     return collections;
   });
-  await page.screenshot({ path: `./${Math.random()}scrapingbee_homepage.jpg` });
-  await page.close();
 
   // filter
   const filteredCollections = filterAndSortCollections(
